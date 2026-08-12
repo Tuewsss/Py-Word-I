96d2a5f94b5a88ac9b036f0d8beb5054d86a9529
Mateus Monteiro Mendes <mateusmonmenofc@gmail.com>
2026-08-12 17:45:19 -0300

    Média Aritmética

---
+++ class/aula_007.py
@@ -0,0 +1,4 @@
+nota_1 = float(input("Digite a primeira nota: "))
+nota_2 = float(input("Digite a segunda nota: "))
+media = (nota_1 + nota_2) / 2
+print(f"A média entre {nota_1} e {nota_2} é {media}.")
