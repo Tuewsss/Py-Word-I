015194d4a7fb068b87c778d57ec9bd2b3ab025c2
Mateus Monteiro Mendes <mateusmonmenofc@gmail.com>
2026-08-12 17:47:02 -0300

    Conversos de Medidas

---
+++ class/aula_008.py
@@ -0,0 +1,4 @@
+metros = int(input("Digite um valor em metros: "))
+centimetros = metros * 100
+milimetros = metros * 1000
+print(f"O valor de {metros} metros corresponde a {centimetros} centímetros e {milimetros} milímetros.")
