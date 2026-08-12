52499267f9bc0f147bb89437edcdf4daf697aa8a
Mateus Monteiro Mendes <mateusmonmenofc@gmail.com>
2026-08-12 17:43:10 -0300

    Dobro, Triplo, Raiz Quadrada

---
+++ class/aula_006.py
@@ -0,0 +1,5 @@
+numero = int(input("Digite um número: "))
+dobro = numero * 2
+triplo = numero * 3
+raiz_quadrada = numero ** (1/2)
+print(f"O dobro de {numero} é {dobro}, o triplo é {triplo} e a raiz quadrada é {raiz_quadrada:.2f}.")
